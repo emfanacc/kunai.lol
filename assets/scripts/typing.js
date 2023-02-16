9b0e4cb84e1cbc1d115996e13d9acba2c085a895
vault <120114398+emfanacc@users.noreply.github.com>
2023-02-16 10:09:24 -0800

    Add files via upload

---
+++ assets/scripts/typing.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.typing', {
+    strings: [
+        'Private Bio Links', 
+        'Friend Group',
+        'Case Closed',
+        '♡'
+    ],
+    startDelay: 2000,
+    typeSpeed: 50,
+    backSpeed: 80,
+    smartBackspace: true
+  });

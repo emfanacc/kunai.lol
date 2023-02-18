349bb5efc838bd91527f55c5c4250024e83f7e0c
vault <120114398+emfanacc@users.noreply.github.com>
2023-02-18 10:54:21 -0800

    Add files via upload

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -28,3 +28,20 @@ document.onkeydown=function(e)
 document.addEventListener('contextmenu', (e) => {
     e.preventDefault();
 })
+
+// Animated title bar 
+
+(function titleScroller(text) {
+    document.title = text;
+    setTimeout(function () {
+      titleScroller(text.substr(1) + text.substr(0, 1));
+    }, 350);
+  }("Vault is <3 "));
+
+// Google analytics 
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WS0FPKJ5BR');

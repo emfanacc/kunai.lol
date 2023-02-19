591ed7c06d64e2a87c5a88ca7e675df968c3428d
vault <120114398+emfanacc@users.noreply.github.com>
2023-02-19 00:28:16 -0800

    Add files via upload

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -29,11 +29,37 @@ document.addEventListener('contextmenu', (e) => {
     e.preventDefault();
 })
 
+// Text Copier (Discord)
+
+let copydisc = document.getElementById("copyclipboard");
+let disc = document.getElementById("disc");
+disc.addEventListener("click", function() {
+    navigator.clipboard.writeText(copydisc.textContent);
+    copydisc.textContent = "Copied"
+})
+
+// Welcomer? idk what to call it
+
+var audio = document.getElementById('audio');
+
+document.getElementById("enter").addEventListener("click", function() {
+    
+    const target = document.getElementById("enter");
+
+    target.style.opacity = '0';
+    target.addEventListener('transitionend', () => target.remove());
+
+    if (typeof window.orientation == 'undefined') { 
+        audio.volume=0.2;
+        audio.play();
+    }
+});
+
 // Animated title bar 
 
-(function titleScroller(text) {
+/*(function titleScroller(text) {
     document.title = text;
     setTimeout(function () {
       titleScroller(text.substr(1) + text.substr(0, 1));
     }, 350);
-  }("Vault is <3 "));
+  }("Vault is <3 "));*/
